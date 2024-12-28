80594b6a3366a61b8cb0366fd11b2a75a2fa0b24
Nubicusit <alan.nubicus@gmail.com>
2024-12-28 13:22:11 +0530

    added fonts
    Added custom font EXO2

--- resources/views/includes/inc.blade.php
+++ resources/views/includes/inc.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ZoroKart Home Page</title>
     <script src="https://kit.fontawesome.com/29d1847fa7.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{ asset('css/style.css')}}">
